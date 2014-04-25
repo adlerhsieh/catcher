08c348f88f25d58e0dedbb44b3aeaec951e9fff6
nkj20932 <nkj20932@hotmail.com>
2014-04-26 01:47:23 +0800

    building game screen
    creating status bar
    creating fixed quiz-window and pause screen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require_tree .
 var switch_id = 0;
+var current_opacity = 0;
 var counter = setInterval(timer, 30);
 
 //按下暫停按鈕，切換按鈕顏色，佈景顏色，停止棒條
@@ -26,7 +27,10 @@ function pause_switch() {
 		$('#pause').attr('id', 'continue');	
 		//切換佈景顏色
 		$('.quiz-window').css('background-color', '#979292');
-		$('.quiz-window').css('opacity', '0.85');
+		$('.quiz-window').css('opacity', '0');
+		current_opacity = $('.quiz-window-2').css('opacity');
+		$('.quiz-window-2').css('opacity', '0');
+		$(".quiz-window-3").css('display', 'static');
 		//停止棒條
 		clearInterval(counter);
 		switch_id = 1;
@@ -37,18 +41,32 @@ function pause_switch() {
 		//切換佈景顏色
 		$('.quiz-window').css('background-color', 'none');
 		$('.quiz-window').css('opacity', '1');
+		$('.quiz-window-2').css('opacity', current_opacity);
+		$(".quiz-window-3").css('display', 'none');
+		counter = setInterval(timer, 30);
 		switch_id = 0;
 	};
 };
 
-function continue_switch() {
-	
-};
-
+//時間棒條
 function timer() {
 	$(".time-bar").animate({width: "-=1"}, 0);
+	//答案逐漸顯露出來
+	$(".quiz-window-2").animate({opacity: "+=.0005"}, 0);
+	if ($(".time-bar").width() < 1) {
+		$(".time-bar").css('opacity', 0);
+	} else {
+		$(".time-bar").css('opacity', 1);
+	};
 };
 
 $(document).ready(function() {
-	
+	$(".quiz-window-2").css('margin-left' , $(".quiz-window").css('margin-left'));
+	$(".quiz-window-3").css('margin-left' , $(".quiz-window").css('margin-left'));
+	$(".quiz-window-3").css('display', 'none');
+});
+
+$(window).resize(function(){
+	$(".quiz-window-2").css('margin-left' , $(".quiz-window").css('margin-left'));
+	$(".quiz-window-3").css('margin-left' , $(".quiz-window").css('margin-left'));
 });
