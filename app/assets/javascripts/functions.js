3ec2f1897f92b65dc862a01ff8a16a0e1fe9d121
nkj20932 <nkj20932@hotmail.com>
2014-05-18 12:08:08 +0800

    completing stage3 & handling revealing speed too slow

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -1,18 +1,25 @@
 //正確答案播放音效&動畫
 function correct_answer() {
 	stage_scoring = stage_scoring + 1;
-	$('#stage_score').text("得分：" + stage_scoring);
+	current_right_answer_number = 0;
 	$(".quiz-window-2").css('opacity', 1);
 	//if the q is in section 1 or 2 then draw a border for right answer
 	if (qs[current_q_number].backgroundImage.indexOf('_') == -1) {
 		$('.right_answer').css('border', '5px solid yellow');
+	} else {
+		//unbind adding right answer function
+		$('.right_answer_multiple').unbind('click', adding_correct_answer);
 	}
+	$('#stage_score').text("得分：" + stage_scoring);
 	$(".right_answer").unbind('click' , correct_answer);
 	$(".quiz-window-2").unbind('click' , wrong_answer);
 	$('.correct').css("display", "block");
-	clearInterval(counter);
 	$('#pause').attr('disabled', true);
-	$('.time-bar').animate({width: "340px"}, 800);
+	//check if this is stage 3, if yes, the timer will not stop
+	if (window.location.pathname != '/stage3') {
+		clearInterval(counter);
+		$('.time-bar').animate({width: "340px"}, 800);
+	}
 	setTimeout(function(){change_quiz()}, 1000);
 };
 
@@ -21,36 +28,67 @@ function wrong_answer() {
 	$(".right_answer").unbind('click' , correct_answer);
 	$(".quiz-window-2").unbind('click' , wrong_answer);
 	$('.wrong').css("display", "block");
-	clearInterval(counter);
 	$('#pause').attr('disabled', true);
-	$('.time-bar').animate({width: "340px"}, 800);
+	//check if this is stage 3, if yes, the timer will not stop
+	if (window.location.pathname != '/stage3') {
+		clearInterval(counter);
+		$('.time-bar').animate({width: "340px"}, 800);
+	}
+	if (qs[current_q_number].backgroundImage.indexOf('_') != -1) {
+		$('.right_answer_multiple').unbind('click', adding_correct_answer);
+	}
 	setTimeout(function(){change_quiz()}, 1000);
 	//make sure users don't have  the following variable > 1 at the beginning of the next r section q
 	current_right_answer_number = 0;	
-};
-
-function time_up() {
-	$(".right_answer").unbind('click' , correct_answer);
-	$(".quiz-window-2").unbind('click' , wrong_answer);
-	$(".instruction_point").css('display', 'none');
-	$('.wrong').css("display", "block");
-	clearInterval(counter);
-	$('#pause').attr('disabled', true);
-	$('.time-bar').animate({width: "340px"}, 800);
-	setTimeout(function(){change_quiz()}, 1000);			
 };
 
 function adding_correct_answer() {
 	$(this).unbind('click', adding_correct_answer);
 	$(this).css('border', '5px solid yellow');
 	//minus 1 because of the "if" runs earlier than the plus
-	if (current_right_answer_number < qs[current_q_number].right_answer_number -1 ) {
+	if (current_right_answer_number < qs[current_q_number].right_answer_number -1) {
 		current_right_answer_number = current_right_answer_number + 1;
+		console.log(current_right_answer_number);
 	} else {
 		correct_answer();
-		current_right_answer_number = 0;
+
 	}
 }
+
+//時間棒條
+function timer() {
+	$(".time-bar").animate({width: "-=1"}, 0);
+	//答案逐漸顯露出來
+	$(".quiz-window-2").animate({opacity: revealing_speed}, 0);
+	if ($(".time-bar").width() < 1) {
+		time_up();
+	} else {
+		$(".time-bar").css('opacity', 1);
+	};
+};
+
+function time_up() {
+	$(".right_answer").unbind('click' , correct_answer);
+	$(".quiz-window-2").unbind('click' , wrong_answer);
+	$(".instruction_point").css('display', 'none');
+	clearInterval(counter);
+	$('#pause').attr('disabled', true);
+	if (window.location.pathname == '/stage3') {
+		$('.quiz-window').css('opacity', '0');
+		$('.quiz-window-2').css('opacity', '0');
+		$('.correct').css("display", "none");
+		$('.wrong').css("display", "none");
+		$(".right_answer_multiple").css('border', 'none');
+		$(".right_answer_multiple").css('display', 'none');
+		$(".quiz-window-3").css('display', 'static');
+		$(".quiz-window-3").css('background-image', 'url("/assets/stage_done_time_up.png")');
+		$("#back-to-menu-id-2").css('display', 'block');
+	} else {
+		$('.time-bar').animate({width: "340px"}, 800);
+		$('.wrong').css("display", "block");
+		setTimeout(function(){change_quiz()}, 1000);
+	}			
+};
 
 function position_right_answer() {
 	if (qs[current_q_number].backgroundImage.indexOf('_') != -1) {
@@ -76,24 +114,16 @@ function position_right_answer() {
 			$("#right_answer_id_5").css('width' , qs[current_q_number].right_answer5_width );
 			$("#right_answer_id_5").css('height' , qs[current_q_number].right_answer5_height );
 		}
+		$(".right_answer").css('marginLeft' , 0 );
+		$(".right_answer").css('marginTop' , 0 );
+		$(".right_answer").css('width' , 0 );
+		$(".right_answer").css('height' , 0 );
 	} else {
 		$(".right_answer").css('marginLeft' , parseInt($(".quiz-window").css('marginLeft')) + qs[current_q_number].marginLeft );
 		$(".right_answer").css('marginTop' , qs[current_q_number].marginTop );
 		$(".right_answer").css('width' , qs[current_q_number].width );
 		$(".right_answer").css('height' , qs[current_q_number].height );
 	}
-};
-
-//時間棒條
-function timer() {
-	$(".time-bar").animate({width: "-=1"}, 0);
-	//答案逐漸顯露出來
-	$(".quiz-window-2").animate({opacity: revealing_speed}, 0);
-	if ($(".time-bar").width() < 1) {
-		time_up();
-	} else {
-		$(".time-bar").css('opacity', 1);
-	};
 };
 
 function position_windows() {
@@ -134,7 +164,7 @@ function pause_switch() {
 		$(".quiz-window-3").css('display', 'static');
 		$(".back-to-menu").css('display', 'block');
 		$(".instruction_point").css('display', 'none');
-		$(".right_answer_multiple").css('border', 'none');
+		$(".right_answer_multiple").css('display', 'none');
 		//停止棒條
 		clearInterval(counter);
 		switch_id = 1;
@@ -149,19 +179,11 @@ function pause_switch() {
 		$('.quiz-window-2').css('opacity', current_opacity);
 		$(".quiz-window-3").css('display', 'none');
 		$(".back-to-menu").css('display', 'none');
+		$(".right_answer_multiple").css('display', 'block');
 		counter = setInterval(timer, stage_time);
 		switch_id = 0;
 	};
 };
-
-//function show_mouse_position() {
-//	var mouse_position = [0,0];
-//	mouse_position[0] = event.pageX - parseInt($(".quiz-window").css('marginLeft')) - 15;
-//	mouse_position[1] = event.pageY - 15;
-//	$(".quiz-window").append("<div class='mouse_position'></div>");
-//	$('.mouse_position').css('marginLeft', mouse_position[0]);
-//	$('.mouse_position').css('marginTop', mouse_position[1]);
-//}
 
 function change_quiz() {
 	$('.right_answer').css('border', 'none');
@@ -187,25 +209,26 @@ function change_quiz() {
 		$('.wrong').css("display", "none");
 		position_right_answer();
 		revealing_speed = qs[current_q_number].revealing_speed;
-		counter = setInterval(timer, stage_time);
+		//check if this is stage 3, if yes, the timer will not reactivate
+		//The first activation of stage 3 timer is in stage3.html.erb
+		if (window.location.pathname != '/stage3') {
+			counter = setInterval(timer, stage_time);
+		}
 	} else {
 		$('.quiz-window').css('opacity', '0');
 		$('.quiz-window-2').css('opacity', '0');
 		$('#pause').attr('disabled', true);
 		$('.correct').css("display", "none");
 		$('.wrong').css("display", "none");
+		clearInterval(counter);
 		$(".quiz-window-3").css('display', 'static');
-			if (user_current_stage_score > 4) {
-				$(".quiz-window-3").css('background-image', 'url("/assets/stage_done.png")');
-			} else if (stage_scoring > 4) {
-				if (window.location.pathname == '/stage3') {
-					$(".quiz-window-3").css('background-image', 'url("/assets/stage_done.png")');
-				} else {
-					$(".quiz-window-3").css('background-image', 'url("/assets/stage_done_win.png")');
-				}
-			} else {
-				$(".quiz-window-3").css('background-image', 'url("/assets/stage_done_lose.png")');
-			};
+		if (window.location.pathname == '/stage3' || user_current_stage_score > 4) {
+			$(".quiz-window-3").css('background-image', 'url("/assets/stage_done.png")');
+		} else if (stage_scoring > 4) {
+			$(".quiz-window-3").css('background-image', 'url("/assets/stage_done_win.png")');
+		} else {
+			$(".quiz-window-3").css('background-image', 'url("/assets/stage_done_lose.png")');
+		}
 		$(".back-to-menu").css('display', 'none');
 		$("#back-to-menu-id-2").css('display', 'static');
 	};
